ef0748b44cba2f9cc9f88fe62b57d1b8753d81e3
Caleb Mouritsen <caleb.mouritsen@tooeleschools.org>
2018-09-13 09:54:35 -0600

    get_name_Python

---
+++ get_nameExample.py
@@ -0,0 +1,32 @@
+#Caleb Mouritsen
+#9/13/18
+#get name function
+
+#def get_name():
+
+#ask user for name
+    #name = input("what is your name?: ")
+#verify name
+    
+#display name
+#print("the name you entered was", name)
+
+
+
+#print("this is our function")
+#get_name()
+
+
+#Finding the area of a circle
+
+def AreaofCircle():
+    
+    pi = 3.14159265
+
+#1 get a radius
+    radius = input("what is the radius of our circle?: ")
+#2 compute the area
+    radius = float(radius)
+    area = radius*radius*pi
+#3 display the area
+    print("the area of the circle is: ", area)
